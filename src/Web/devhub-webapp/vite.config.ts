48f5df1c47126695660c5aa1bb9c30079661daa0
vasiya99 <vasiya99@gmail.com>
2025-04-16 16:47:37 +1200

    Removed the alias in vite.config.ts build pipeline fails.

--- src/Web/devhub-webapp/vite.config.ts
+++ src/Web/devhub-webapp/vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
   },
   base: '/', // Ensures correct relative paths
 })
